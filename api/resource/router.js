69984656358074d74b2dc0b8e93a37434088ed50
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-19 10:23:55 -0500

    reinitialized db

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,33 @@
 // build your `/api/resources` router here
+
+// build your `/api/projects` router here
+
+const express = require("express");
+const router = express.Router();
+
+
+
+const Resources = require('./model.js');
+
+
+
+router.get('/', (req, res) => {
+    Resources.getAll()
+    .then(resources => {
+        res.status(200).json(resources)
+    })
+});
+
+router.post('/', async (req, res) => {
+    await Resources.insert(req.body)
+    .then(resource => {
+        res.status(200).json(resource)
+    })
+    .catch(err => {
+        res.status(500).json(err.message)
+    })
+});
+
+
+module.exports = router;
+
